e51eea8581907c768ed01152c28c6104a768542f
Federico "Lox" Lucignano <federico.lox@gmail.com>
2012-07-15 22:17:18 +0200

    support synchronous in require implementation + special credits

--- src/modil.js
+++ src/modil.js
@@ -4,7 +4,6 @@
  * A no-frills, lightweight and fast AMD implementation for modular Javascript projects.
  *
  * @author Federico "Lox" Lucignano <https://plus.google.com/117046182016070432246>
- * @author Jakub Olek <https://plus.google.com/112565259111817320425>
  * @see https://github.com/federico-lox/modil.js
  * @see http://requirejs.org/docs/api.html for example and docs until the official docs for modil ain't ready
  */
@@ -24,10 +23,10 @@
 	/**
 	 * @private
 	 */
-	function process(name, requestId){
-		var module = modules[name],
-			//manage the process chain per request call since it's async
-			pid = processing[requestId],
+	function process(id, reqId){
+		var module = modules[id],
+			//manage the process chain per require call since it can be an async call
+			pid = processing[reqId],
 			dependencies,
 			chain = '',
 			x = 0,
@@ -40,20 +39,20 @@
 
 		if(!pid){
 			pid = {length: 0};
-		}else if(pid[name]){
+		}else if(pid[id]){
 			for(p in pid){
 				if(p !== 'length'){
 					chain += p + '->';
 				}
 			}
 
-			throw "circular dependency: " + chain + name;
+			throw "circular dependency: " + chain + id;
 		}
 
-		pid[name] = yes;
+		pid[id] = yes;
 		pid.length++;
-		processing[requestId] = pid;
-		module = definitions[name];
+		processing[reqId] = pid;
+		module = definitions[id];
 
 		if(module && module.def){
 			if(module.dep instanceof arrType){
@@ -61,19 +60,19 @@
 				y = module.dep.length;
 
 				for(; x < y; x++){
-					dependencies[x] = process(module.dep[x], requestId);
+					dependencies[x] = process(module.dep[x], reqId);
 				}
 			}
 
-			modules[name] = module = module.def.apply(context, dependencies);
+			modules[id] = module = module.def.apply(context, dependencies);
 		}
 
-		delete definitions[name];
-		delete pid[name];
+		delete definitions[id];
+		delete pid[id];
 		pid.length--;
 
 		if(!pid.length){
-			delete processing[requestId];
+			delete processing[reqId];
 		}
 
 		return module;
@@ -82,9 +81,9 @@
 	/**
 	 * @public
 	 */
-	context.define = def = function(name, dependencies, definition){
-		if(typeof name !== strType){
-			throw "module name missing or not a string";
+	context.define = def = function(id, dependencies, definition){
+		if(typeof id !== strType){
+			throw "module id missing or not a string";
 		}
 
 		//no dependencies array, it's actually the definition
@@ -94,55 +93,57 @@
 		}
 
 		if(!definition){
-			throw "module " + name + " is missing a definition";
+			throw "module " + id + " is missing a definition";
 		}
 
 		if(definition instanceof funcType){
-			definitions[name] = {def: definition, dep: dependencies};
+			definitions[id] = {def: definition, dep: dependencies};
 		}else{
-			modules[name] = definition;
+			modules[id] = definition;
 		}
 	};
 
 	/**
-	 * required for UMD checks and to let jQuery register correctly
-	 * @see https://github.com/amdjs/amdjs-api/wiki/jQuery-and-AMD
+	 * declare support for the AMD spec
 	 */
 	def.amd = {
+		/**
+		 * @see https://github.com/amdjs/amdjs-api/wiki/jQuery-and-AMD
+		 */
 		jQuery: yes
 	};
 
 	/**
 	 * @public
 	 */
-	context.require = function(name, callback){
-		//execute asynchronously
-		setTimeout(function(){
-			var isArray = name instanceof arrType,
-				id = Math.random();
-				m = [];
+	context.require = function(ids, callback){
+		var ret;
 
-			if(typeof name !== strType && !isArray){
-				throw "module name missing or not valid";
-			}
+		if(!callback && typeof ids === strType){
+			//execute synchronously as per CommonJS spec
+			ret = process(ids, Math.random());
+		}else if(callback instanceof Function && ids instanceof Array){
+			//execute asynchronously
+			setTimeout(function(){
+				var reqId = Math.random();
+					m = [];
 
-			if(isArray){
 				var x = 0,
-					y = name.length;
+					y = ids.length;
 
 				for(; x < y; x++){
-					m[x] = process(name[x], id);
+					m[x] = process(ids[x], reqId);
 				}
-			}else{
-				m[0] = process(name, id);
-			}
 
-			if(callback instanceof funcType){
 				callback.apply(context, m);
-			}
-		}, 0);
+			}, 0);
+		}else{
+			throw "Invalid require invokation";
+		}
+
+		return ret;
 	};
 
 	//clear up temporary refs
-	d = nil;
+	def = nil;
 }(this));
